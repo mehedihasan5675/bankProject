49c175c7442c6c02661c49b3c78fcb10176c66af
MD MEHEDI HASAN <mdchanchal5675@gmail.com>
2023-02-13 17:46:20 +0600

    updated validation

--- js/login.js
+++ js/login.js
@@ -6,13 +6,36 @@ let registerpass='';
 document.getElementById('sign_up_btn').addEventListener('click',function(){
    const email=document.getElementById('input_email_sign').value;
    const emailPass=document.getElementById('input_password_sign').value;
+
+
+
+
+ // sign up part validation
+ //=================================
+if(!email /*email ==false */){
+    document.getElementById('sign_up_success').innerText='Email must be provide';
+    // alert('Email must be provide');return
+}
+else if(!email.includes('@')/*email.includes('@') ==false */ || !email.endsWith('.com') /*email.endsWith('.com') ==false */){
+    document.getElementById('sign_up_success').innerText='Invalid email : try Again';
+    // alert('Invalid : try Again'); return
+}
+else if(emailPass.length < 8){
+    document.getElementById('sign_up_success').innerText='Password must be at least 8 characters long';
+    // alert('Password must be at least 8 characters long');return    
+}else{
+    document.getElementById('sign_up_success').innerText='Registration successfully Done!';
+document.getElementById('sign_up_success').style.color='#00FF00'
+}
+//
+//==============================
+
 registermail=email;
 registerpass=emailPass;
 document.getElementById('input_email_sign').value='';
 document.getElementById('input_password_sign').value='';
 
 
-document.getElementById('sign_up_success').innerText='Registration successfully Done!'
 });
 
 
@@ -23,7 +46,7 @@ document.getElementById('sign_up_success').innerText='Registration successfully 
 const loginBtn = document.getElementById('login_btn');
 loginBtn.addEventListener('click',function(){
 const inputEmailField = document.getElementById('input_email_log');
-const inpurEmailText = inputEmailField.value;
+const inputEmailText = inputEmailField.value;
 
 // console.log(inpurEmailText);
 
@@ -32,11 +55,21 @@ const inputPassText = inputPassField.value;
 
 // inputEmailField.value='';
 inputPassField.value='';
+console.log(registermail,registerpass);
 
-if(inpurEmailText === registermail && inputPassText===registerpass){
+// if(inputEmailText === registermail && inputPassText===registerpass){
+//     window.location.href='bank.html'
+// }
+if(registermail && registerpass){
+    if(inputEmailText === registermail && inputPassText===registerpass){
     window.location.href='bank.html'
 }else{
     document.getElementById('login_success').innerText='Please enter your correct information!'
+}
+}
+
+else{
+    document.getElementById('login_success').innerText='Please fill up your field'
 }
 
 
@@ -51,3 +84,16 @@ if(inpurEmailText === registermail && inputPassText===registerpass){
 
 
 
+// if(!textEmail){
+//     alert('Email must be provide');
+//     return;
+// }
+// if(!textEmail.includes('@') || !textEmail.endsWith('.com')){
+//     alert('Invalid : try Again');
+//     return;
+// }
+// if(textPassword.length < 8){
+//     alert('Password must be at least 8 characters long');
+//     return;
+    
+// }
